29f30181c910b2377fe1fe08f7fd38f656942a19
tapcloud2019 <tapcloud2019@gmail.com>
2023-01-28 15:49:19 +0800

    extract email, append pf, send calendar invite based on new filename format

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 # Script to convert json to xml and dump it to a directory.
+from dateutil import parser
 from icalendar import vCalAddress
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from json2xml import json2xml
 import json as j
 import shutil
 import os
-
-#import email.MIMEText
-#import email.MIMEBase
-#from email.MIMEMultipart import MIMEMultipart
 import email.mime.text
 import email.mime.base
 import email.mime.multipart
-
 import datetime as dt
 import icalendar
 import pytz
@@ -25,8 +19,11 @@ def inbox_path():
 def sap_bucket():
     return ".\sap_bucket"
 
-def processed_path():
-    return ".\processed"
+def processed_sap_path():
+    return ".\processed-sap"
+
+def processed_cal_path():
+    return ".\processed-cal"
 
 def handle_xml_type(filepath, filename):
 
@@ -40,8 +37,7 @@ def handle_xml_type(filepath, filename):
         process_for_sap(filepath, filename)
     elif "CALENDAR" in filename:
         print("\tCALENDAR json detected")
-        process_for_outlook(filePath, filename)
-        send_invite()
+        process_for_calendar(filePath, filename)
 
 def process_leave(filepath):
     with open(filepath, "r") as file:
@@ -70,33 +66,63 @@ def process_leave(filepath):
 def process_dots(filepath):
     print('\tDOTS handling still undefined')
 
-def process_for_outlook(filepath, filename):
+def process_for_calendar(filepath, filename):
+    # retrieve email from filename format: pf,email,datetime,(CloudHR)_TYPE.TXT
+    recipient = filename.split(",")[1]
+
     with open(filepath, "r") as file:
         filedata = file.read()
         jsonstring = j.loads(filedata)
-
-    print(jsonstring)
 
     eventList = []
     for events in jsonstring:
         event = {}
         for key, val in events.items():
             event[key] = val
-            #print(key, val)
-            
+
         eventList.append(event)
 
+    for event in eventList:
+        end = event["End"]
+        start = event["Start"]
+        subject = event["Subject"]
+        venue = event["Venue"]
+
+        #Retrieve start date & time
+        startSplit = parser.parse(start).isoformat().split("T") #convert 1/4/2023 5:00 PM to 2023-04-01T17:00:00
+        startDMY = startSplit[0].split("-")
+        startHMS = startSplit[1].split(":")
+        startY = startDMY[0]
+        startM = startDMY[1]
+        startD = startDMY[2]
+        startHH = startHMS[0]
+        startMM = startHMS[1]
+
+        #Retrieve end date & time
+        endSplit = parser.parse(end).isoformat().split("T")
+        endDMY = endSplit[0].split("-")
+        endHMS = endSplit[1].split(":")
+        endY = endDMY[0]
+        endM = endDMY[1]
+        endD = endDMY[2]
+        endHH = endHMS[0]
+        endMM = endHMS[1]
+
+        send_invite(recipient,endY,endM,endD,endHH,endMM,startY,startM,startD,startHH,startMM,subject,venue)
+
+    # Move json file to processed folder
+    shutil.move(filepath, os.path.join(processed_cal_path(), filename))
+
 def process_for_sap(filepath, filename):
-    # Use a breakpoint in the code line below to debug your script.
-    # Press Ctrl+F8 to toggle the breakpoint.
     with open(filepath, "r") as file:
-        jsonstring = j.load(file)
+        jsonObj = j.load(file)
 
-    # Convert json to xml
-    xmlstring = json2xml.Json2xml(jsonstring).to_xml()
+    # add pf from filename format: pf,email,datetime,(CloudHR)_TYPE.TXT
+    jsonObj[0]["pf"] = filename.split(",")[0]
 
-    # Write to xml file
-    xmlfilename = filename.replace('.txt', '.xml')
+    xmlstring = json2xml.Json2xml(jsonObj).to_xml()
+
+    xmlfilename = filename.replace('.TXT', '.XML')
     with open(xmlfilename, "w") as xmlfile:
         xmlfile.write(xmlstring)
 
@@ -104,108 +130,106 @@ def process_for_sap(filepath, filename):
     shutil.move(xmlfilename, os.path.join(sap_bucket(), xmlfilename))
 
     # Move json file to processed folder
-    shutil.move(filepath, os.path.join(processed_path(), filename))
+    shutil.move(filepath, os.path.join(processed_sap_path(), filename))
 
-# Imagine this function is part of a class which provides the necessary config data
 # https://learnpython.com/blog/working-with-icalendar-with-python/
-# https://learnpython.com/blog/working-with-icalendar-with-python/
-#def send_appointment(self, attendee_email, organiser_email, subj, description, location, start_hour, start_minute):
-def send_invite():
-  # Timezone to use for our dates - change as needed
-  tz = pytz.timezone("Europe/London")
-  #start = tz.localize(dt.datetime.combine(self.date, dt.time(start_hour, start_minute, 0)))
-  # Build the event itself
-  cal = icalendar.Calendar()
-  cal.add('prodid', '-//Unclass calendar event //example.com//')
-  cal.add('version', '2.0')
-  cal.add('method', "REQUEST")
-  event = icalendar.Event()
-  attendee_email = vCalAddress('MAILTO:rdoe@example.com')
-  event.add('attendee', attendee_email, encode=0)
-  organiser_email = vCalAddress('MAILTO:service@example.com')
-  event.add('organizer', organiser_email)
-  # event.add('attendee', attendee_email)
-  # event.add('organizer', organiser_email)
-  event.add('status', "confirmed")
-  event.add('category', "Event")
-  event.add('summary', "Meeting with vendor")
-  #event.add('summary', subj)
-  event.add('description', "Meeting with vendor")
-  #event.add('description', description)
-  event.add('location',"Jurong East")
-  #event.add('location', location)
-  event.add('dtstart', dt.datetime(2022, 1, 25, 8, 0, 0, tzinfo=pytz.utc))
-  event.add('dtend', dt.datetime(2022, 1, 25, 10, 0, 0, tzinfo=pytz.utc))
-  #event.add('dtstart', start)
-  #event.add('dtend', tz.localize(dt.datetime.combine(self.date, dt.time(start_hour + 1, start_minute, 0))))
-  #event.add('dtstamp', tz.localize(dt.datetime.combine(self.date, dt.time(6, 0, 0))))
-  #event['uid'] = self.get_unique_id() # Generate some unique ID
-  event['uid'] = 1234
-  event.add('priority', 5)
-  event.add('sequence', 1)
-  event.add('created', tz.localize(dt.datetime.now()))
+# https://www.baryudin.com/blog/entry/sending-outlook-appointments-python/
+def send_invite(recipient, endy, endm, endd, endhh, endmm, starty, startm, startd, starthh, startmm, subject, venue):
+    # Build the event
+    cal = icalendar.Calendar()
+    cal.add('prodid', '-//Unclass calendar event //example.com//')
+    cal.add('version', '2.0')
+    cal.add('method', "REQUEST")
 
-  # Add a reminder
-  #alarm = icalendar.Alarm()
-  #alarm.add("action", "DISPLAY")
-  #alarm.add('description', "Reminder")
-  # The only way to convince Outlook to do it correctly
-  #alarm.add("TRIGGER;RELATED=START", "-PT{0}H".format(reminder_hours))
-  #event.add_component(alarm)
+    attendee = 'MAILTO:' + recipient
+    event = icalendar.Event()
+    attendee_email = vCalAddress(attendee)
+    event.add('attendee', attendee_email, encode=0)
+    organiser_email = vCalAddress('MAILTO:service@example.com')
+    event.add('organizer', organiser_email)
+    event.add('status', "confirmed")
+    event.add('category', "Event")
+    event.add('summary', subject)
+    event.add('description', subject)
+    event.add('location', venue)
+    event.add('dtstart', dt.datetime(int(starty), int(startm), int(startd), int(starthh), int(startmm), 0, tzinfo=pytz.utc))
+    event.add('dtend', dt.datetime(int(endy), int(endm), int(endd), int(endhh), int(endmm), 0, tzinfo=pytz.utc))
+    event['uid'] = 1234
+    event.add('priority', 5)
+    event.add('sequence', 1)
 
-  cal.add_component(event)
+    #event.add('attendee', attendee_email)
+    #event.add('organizer', organiser_email)
+    #event.add('description', "Meeting with vendor")
+    #event.add('location',"Jurong East")
+    #event.add('dtstart', dt.datetime(2022, 1, 25, 8, 0, 0, tzinfo=pytz.utc))
+    #event.add('dtend', dt.datetime(2022, 1, 25, 10, 0, 0, tzinfo=pytz.utc))
+    #event.add('dtstart', start)
+    #event.add('dtend', tz.localize(dt.datetime.combine(self.date, dt.time(start_hour + 1, start_minute, 0))))
+    #event.add('dtstamp', tz.localize(dt.datetime.combine(self.date, dt.time(6, 0, 0))))
+    #event['uid'] = self.get_unique_id() # Generate some unique ID
+    #event.add('created', tz.localize(dt.datetime.now()))
+    # Add a reminder
+    #alarm = icalendar.Alarm()
+    #alarm.add("action", "DISPLAY")
+    #alarm.add('description', "Reminder")
+    # The only way to convince Outlook to do it correctly
+    #alarm.add("TRIGGER;RELATED=START", "-PT{0}H".format(reminder_hours))
+    #event.add_component(alarm)
 
-  # Build the email message and attach the event to it
-  msg = email.mime.multipart.MIMEMultipart("alternative")
-  #msg["Subject"] = subj
-  msg["Subject"] = "Meeting with vendor"
-  msg["From"] = organiser_email
-  msg["To"] = attendee_email
-  msg["Content-class"] = "urn:content-classes:calendarmessage"
+    cal.add_component(event)
 
-  #msg.attach(email.MIMEText.MIMEText(description))
-  msg.attach(email.mime.text.MIMEText("Meeting with vendor"))
+    # Build the email message and attach the event to it
+    msg = email.mime.multipart.MIMEMultipart("alternative")
+    msg["Subject"] = subject
+    msg["From"] = organiser_email
+    msg["To"] = attendee_email
+    msg["Content-class"] = "urn:content-classes:calendarmessage"
+    msg.attach(email.mime.text.MIMEText(subject))
 
-  filename = "invite.ics"
-  part = email.mime.base.MIMEBase('text', "calendar", method="REQUEST", name=filename)
-  part.set_payload( cal.to_ical() )
-  #email.encoders.encode_base64(part)
-  part.add_header('Content-Description', filename)
-  part.add_header("Content-class", "urn:content-classes:calendarmessage")
-  part.add_header("Filename", filename)
-  part.add_header("Path", filename)
-  msg.attach(part)
+    filename = "invite.ics"
+    part = email.mime.base.MIMEBase('text', "calendar", method="REQUEST", name=filename)
+    part.set_payload( cal.to_ical() )
+    #email.encoders.encode_base64(part)
+    part.add_header('Content-Description', filename)
+    part.add_header("Content-class", "urn:content-classes:calendarmessage")
+    part.add_header("Filename", filename)
+    part.add_header("Path", filename)
+    msg.attach(part)
 
-  # Send the email out
-  #s = smtplib.SMTP('localhost')
-  #s.sendmail(msg["From"], [msg["To"]], msg.as_string())
-  #s.quit()
+    # Send the email out
+    #s = smtplib.SMTP('localhost')
+    #s.sendmail(msg["From"], [msg["To"]], msg.as_string())
+    #s.quit()
 
-  # Write to disk
-  directory = Path.cwd() / 'MyCalendar'
-  try:
-      directory.mkdir(parents=True, exist_ok=False)
-  except FileExistsError:
-      print("Folder already exists")
-  else:
-      print("Folder was created")
+    # Write calendar invite to disk
+    directory = Path.cwd() / 'processed-cal'
+    try:
+        directory.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        print("Folder already exists")
+    else:
+        print("Folder was created")
 
-  f = open(os.path.join(directory, 'example.ics'), 'wb')
-  f.write(cal.to_ical())
-  f.close()
+    filename = endy + endm + endd + endhh + endmm + starty + startm + startd + starthh + startmm + ".ics"
+    filepath = processed_cal_path() + "/" + filename
 
-  e = open('MyCalendar/example.ics', 'rb')
-  ecal = icalendar.Calendar.from_ical(e.read())
-  for component in ecal.walk():
-      print(component.name)
-      if component.name == "VEVENT":
-          #print(component.get("name"))
-          print(component.get("description"))
-          print(component.get("summary"))
-          print(component.get("location"))
-          print(component.decoded("dtstart"))
-          print(component.decoded("dtend"))
-  e.close()
+    f = open(os.path.join(directory, filename), 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+    e = open(filepath, 'rb')
+    ecal = icalendar.Calendar.from_ical(e.read())
+    for component in ecal.walk():
+        print(component.name)
+        if component.name == "VEVENT":
+            print(component.get("attendee"))
+            print(component.get("description"))
+            print(component.get("summary"))
+            print(component.get("location"))
+            print(component.decoded("dtstart"))
+            print(component.decoded("dtend"))
+    e.close()
 
 # Main function
 if __name__ == '__main__':
